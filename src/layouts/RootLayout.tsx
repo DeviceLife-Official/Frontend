146a4973c3abd0deaee8ac7a21f91cf50240d679
YuminPark <p353700@gmail.com>
2026-01-11 22:27:16 +0900

    refactor: min-width 너비 수정 및 미사용 폰트 제거

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
   return (
-    <main className="mx-auto min-w-768 max-w-1920 w-full min-h-screen overflow-auto">
+    <main className="mx-auto min-w-1440 max-w-1920 w-full min-h-screen overflow-auto">
       <div className="min-w-max flex flex-col">
         <NavBar />
         <Outlet />
