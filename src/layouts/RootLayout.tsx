feef3314629d89fb66dedb877b60c5ee3cb2dc52
YuminPark <p353700@gmail.com>
2026-01-11 17:32:31 +0900

    fix: 루트 layout 높이 min-h-screen으로 변경

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
   return (
-    <main className="mx-auto min-w-768 max-w-1920 w-full h-screen overflow-auto">
-      <div className="min-w-max min-h-screen flex flex-col">
+    <main className="mx-auto min-w-768 max-w-1920 w-full min-h-screen overflow-auto">
+      <div className="min-w-max flex flex-col">
         <NavBar />
         <Outlet />
       </div>
