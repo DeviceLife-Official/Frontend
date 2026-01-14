2aad0fdea0461ae1549cb1f5a2b6772aa23c48da
YuminPark <p353700@gmail.com>
2026-01-14 16:19:18 +0900

    chore: 조합 생성 페이지 원상복구

--- src/pages/combination/CombinationCreatePage.tsx
+++ src/pages/combination/CombinationCreatePage.tsx
@@ -1,26 +1,5 @@
-import PrimaryButton from '@/components/Button/PrimaryButton';
-
 const CombinationCreatePage = () => {
-  return (
-    <div className="flex flex-col gap-10">
-      <PrimaryButton text="로그인" className="w-440" disabled />
-      <PrimaryButton text="로그인" className="w-440 bg-blue-600 hover:bg-blue-500" />
-
-      <PrimaryButton
-        text="로그인하고 내 조합에 담기"
-        className="w-400 bg-blue-500 hover:bg-blue-400"
-      />
-      <PrimaryButton text="내 조합에 담기" className="w-400 bg-blue-500 hover:bg-blue-400" />
-
-      <PrimaryButton text="완료" className="w-280 bg-blue-600 hover:bg-blue-500" />
-      <PrimaryButton text="다음" className="w-280 bg-blue-500 hover:bg-blue-400" />
-
-      <PrimaryButton text="조합1 에 담기" className="w-280 bg-blue-600 hover:bg-blue-500" />
-      <PrimaryButton text="이미 담은 상품입니다." className="w-280" disabled />
-
-      <PrimaryButton text="새 조합 추가하기" className="w-280 bg-blue-600 hover:bg-blue-500" />
-    </div>
-  );
+  return <div>CombinationCreatePage</div>;
 };
 
 export default CombinationCreatePage;
