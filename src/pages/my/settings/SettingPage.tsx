401f028d78d438109a26015d2c7e0e9ac3f6215e
YuminPark <p353700@gmail.com>
2026-01-17 16:59:42 +0900

    feat: 컨테이너 센터 정렬

--- src/pages/my/settings/SettingPage.tsx
+++ src/pages/my/settings/SettingPage.tsx
@@ -5,17 +5,22 @@ const SettingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="px-160 mt-92 flex flex-col gap-32">
-      <p className="font-heading-2 text-black">설정</p>
-      <div className="flex flex-col gap-8">
-        <SettingRow title="프로필 수정" onClick={() => navigate('/my/settings/profile')} />
-        <SettingRow title="공지사항" onClick={() => navigate('/support/notices')} />
-        <SettingRow
-          title="고객센터/문의하기"
-          onClick={() => navigate('/support/customer-center')}
-        />
-        <SettingRow title="서비스 이용약관" onClick={() => navigate('/support/terms')} />
-        <SettingRow title="개인정보 처리방침" onClick={() => navigate('/support/privacy-policy')} />
+    <div className="min-h-[calc(100vh-80px)] flex items-center">
+      <div className="w-full px-160 flex flex-col gap-32">
+        <p className="font-heading-2 text-black">설정</p>
+        <div className="flex flex-col gap-8">
+          <SettingRow title="프로필 수정" onClick={() => navigate('/my/settings/profile')} />
+          <SettingRow title="공지사항" onClick={() => navigate('/support/notices')} />
+          <SettingRow
+            title="고객센터/문의하기"
+            onClick={() => navigate('/support/customer-center')}
+          />
+          <SettingRow title="서비스 이용약관" onClick={() => navigate('/support/terms')} />
+          <SettingRow
+            title="개인정보 처리방침"
+            onClick={() => navigate('/support/privacy-policy')}
+          />
+        </div>
       </div>
     </div>
   );
