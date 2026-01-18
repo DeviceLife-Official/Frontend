0158877a0c32daef338db19b56eb810ba47c7ac3
Seony777 <addiolsw1@gmail.com>
2026-01-18 14:02:38 +0900

    refactor: SignupButton 스타일 및 레이아웃 수정

--- src/components/Button/SignupButton.tsx
+++ src/components/Button/SignupButton.tsx
@@ -21,18 +21,18 @@ const SignupButton = ({
       onClick={onClick}
       disabled={disabled}
       className={`
-        flex items-center justify-center
-        w-full h-72 px-16 py-24
+        relative flex items-center justify-center
+        w-full h-72 px-24 py-24
         bg-white border border-black rounded-button
         outline-none
         ${disabled ? 'bg-gray-100 cursor-not-allowed' : 'cursor-pointer'}
         ${className}
       `}
     >
-      {/* 아이콘/로고 */}
-      <div className="flex-shrink-0 size-40 flex items-center justify-center">{icon}</div>
+      {/* 아이콘/로고 - 왼쪽 끝 고정 */}
+      <div className="absolute left-24 size-36 flex items-center justify-center">{icon}</div>
 
-      {/* 텍스트 */}
+      {/* 텍스트 - 나머지 공간의 가로 중심 */}
       <span className="flex-1 font-body-2-r text-black text-center">{text}</span>
     </button>
   );
