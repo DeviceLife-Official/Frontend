e213a76bdadd3c78775970361213934e58169379
Seony777 <addiolsw1@gmail.com>
2026-01-20 18:46:52 +0900

    feat: 원형 UI 구현

--- src/pages/auth/OnboardingCompletePage.tsx
+++ src/pages/auth/OnboardingCompletePage.tsx
@@ -1,5 +1,19 @@
 const OnboardingCompletePage = () => {
-  return <div>OnboardingCompletePage</div>;
+  // TODO: 사용자 이름은 전역 상태 또는 API에서 받아오기
+  const userName = '000';
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-white">
+      {/* 원형 컨테이너 */}
+      <div className="flex items-center justify-center size-312 rounded-full bg-white border-shadow-blue-welcome">
+        {/* 환영 메시지 */}
+        <div className="flex items-center gap-6 font-heading-3 text-blue-600">
+          <span>{userName}</span>
+          <span>님, 어서오세요!</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OnboardingCompletePage;
