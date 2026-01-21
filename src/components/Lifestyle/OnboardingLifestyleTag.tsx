828b35b1d32ecab2ca190276ab217f3f218e49cb
Seony777 <addiolsw1@gmail.com>
2026-01-21 12:48:40 +0900

    design: 폰트 수정

--- src/components/Lifestyle/OnboardingLifestyleTag.tsx
+++ src/components/Lifestyle/OnboardingLifestyleTag.tsx
@@ -20,7 +20,7 @@ const OnboardingLifestyleTag = ({
         px-20 py-10 rounded-button bg-white cursor-pointer
         transition-all duration-150
         ${selected
-          ? 'border-shadow-blue font-body-1-sm text-blue-700'
+        ? 'border-shadow-blue font-Body-1-sm text-blue-700'
           : 'border-shadow-black font-body-1-sm text-black hover:text-blue-500'
         }
         ${className}
