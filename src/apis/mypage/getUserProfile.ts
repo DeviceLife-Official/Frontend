41314e636eb77bd640e0704b12f38c133c0875e7
Seony777 <addiolsw1@gmail.com>
2026-01-25 16:53:35 +0900

    refactor: 유저 프로필 쿼리에서 비로그인 상태 허용

--- src/apis/mypage/getUserProfile.ts
+++ src/apis/mypage/getUserProfile.ts
@@ -5,19 +5,15 @@ import { queryKeys } from '@/constants/queryKeys';
 import { hasAuthTokens } from '@/utils/authStorage';
 
 // 유저 정보 조회 API
-export const getUserProfile = async (): Promise<UserProfileResult> => {
+export const getUserProfile = async (): Promise<UserProfileResult | undefined> => {
   const { data } = await axiosInstance.get<UserProfileResponse>('/api/mypage/user-profile');
-  if (!data?.result) {
-    throw new Error('유저 정보가 없습니다.');
-  }
   return data.result;
 };
 
 // 유저 정보 조회 Query
 export const useGetUserProfile = () => {
   const hasTokens = hasAuthTokens();
-
-  return useQuery<UserProfileResult>({
+  return useQuery<UserProfileResult | undefined>({
     queryKey: queryKeys.userProfile,
     queryFn: getUserProfile,
     enabled: hasTokens, // 토큰이 있을 때만 조회
