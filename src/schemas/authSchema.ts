1a59792060853c40410c1cf2b5da2a57e3a5dce6
Seony777 <addiolsw1@gmail.com>
2026-01-31 22:55:00 +0900

    refactor: 휴대폰 번호 스키마 수정

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -23,10 +23,7 @@ export const nameSchema = z
 export const phoneSchema = z
   .string()
   .min(1, '휴대폰 번호를 입력해주세요')
-  .refine(
-    (value) => /^[0-9]+$/.test(value) && value.length >= 10 && value.length <= 11,
-    '휴대폰 번호 형식을 확인해주세요'
-  );
+  .regex(/^[0-9]{10,11}$/, '휴대폰 번호 형식을 확인해주세요');
 
 // 로그인 스키마
 export const loginSchema = z.object({
