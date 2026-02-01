514a398d1f2b733298657331c67ca84e56ad065e
Seony777 <addiolsw1@gmail.com>
2026-02-01 15:37:27 +0900

    fix: hasSubmitted 상태 다시 복원

--- src/components/Auth/FindPasswordStep/Step1Form.tsx
+++ src/components/Auth/FindPasswordStep/Step1Form.tsx
@@ -62,7 +62,7 @@ const Step1Form = ({ onSubmit, onInvalid, isPending, hasSubmitted }: Step1FormPr
           {/* 입력 영역 */}
           <div className="flex flex-col gap-8 w-full">
             <PrimaryInput {...register('email')} type="email" placeholder="이메일(ID)" />
-            {errors.email && (
+            {(hasSubmitted || errors.email) && errors.email && (
               <p className="font-body-3-r text-warning">{errors.email.message}</p>
             )}
           </div>
