5ab1cd4a0c3309e3dfcdd2c9789fed3cc7f9171e
hun <skyyy5347@naver.com>
2026-02-01 13:56:04 +0900

    design: 별 아이콘 사이즈 수정

--- src/pages/my/MyPage.tsx
+++ src/pages/my/MyPage.tsx
@@ -439,7 +439,7 @@ const MyPage = () => {
                             </div>
                             <div className="flex items-center gap-8">
                               <p className="font-heading-3 text-black">{combination.name}</p>
-                              {combination.isMain && <StarIcon className="w-27 h-27 -mt-2" />}
+                              {combination.isMain && <StarIcon className="w-22 h-22 -mt-2" />}
                             </div>
                           </div>
                         </div>
@@ -655,7 +655,7 @@ const MyPage = () => {
                                   </div>
                                   <div className="flex items-center gap-8">
                                     <p className="font-body-1-sm text-black">{combination.name}</p>
-                                    {combination.isMain && <StarIcon className="w-27 h-27 -mt-2" />}
+                                    {combination.isMain && <StarIcon className="w-22 h-22 -mt-2" />}
                                   </div>
                                 </div>
                               )}
@@ -717,7 +717,7 @@ const MyPage = () => {
                                 </div>
                                 <div className="flex items-center gap-8">
                                   <p className="font-body-1-sm text-black">{combination.name}</p>
-                                  {combination.isMain && <StarIcon className="w-27 h-27 -mt-2" />}
+                                  {combination.isMain && <StarIcon className="w-22 h-22 -mt-2" />}
                                 </div>
                               </div>
                               {/* Tags */}
