53b9482bf1784f63791a8bfdebfff7811945b64f
Seony777 <addiolsw1@gmail.com>
2026-02-04 16:23:12 +0900

    refactor: getCombos의 enabled값 수정

--- src/apis/combo/getCombos.ts
+++ src/apis/combo/getCombos.ts
@@ -2,7 +2,7 @@ import { axiosInstance } from '@/apis/axios/axios';
 import type { GetCombosResponse, GetCombosResult } from '@/types/combo/combo';
 import { useQuery } from '@tanstack/react-query';
 import { queryKey } from '@/constants/queryKey';
-import { hasAuthTokens } from '@/utils/auth/authStorage';
+import { useAuth } from '@/hooks/useAuth';
 
 // 조합 목록 조회
 export const getCombos = async (): Promise<GetCombosResult> => {
@@ -11,11 +11,12 @@ export const getCombos = async (): Promise<GetCombosResult> => {
 };
 
 export const useGetCombos = () => {
-  const hasTokens = hasAuthTokens();
+  const { isLoggedIn } = useAuth();
 
   return useQuery<GetCombosResult>({
+
     queryKey: [queryKey.COMBOS],
     queryFn: getCombos,
-    enabled: hasTokens, // 토큰이 있을 때만 호출
+    enabled: isLoggedIn
   });
 };
