86388d20492147fca4efecea0f1c04f4064d1fcd
hun <skyyy5347@naver.com>
2026-02-05 01:55:56 +0900

    design: 기기명이 2줄, 3줄 넘어갈 경우, 기기 상세보기 스펙을 사진과 같은 라인으로 맞추기

--- src/pages/devices/DeviceSearchPage.tsx
+++ src/pages/devices/DeviceSearchPage.tsx
@@ -449,76 +449,78 @@ const DeviceSearchPage = () => {
                   onClick={(e) => e.stopPropagation()}
                 >
                   {/* Content */}
-                  <div className="flex items-start justify-between gap-56">
-                    {/* Left Section */}
-                    <div className="w-400 flex flex-col gap-20">
-                      {/* Name & Price + Image */}
-                      <div className="flex flex-col gap-20">
-                        {/* Name & Price */}
-                        <div className="flex flex-col gap-12">
-                          <p className="font-heading-1 text-blue-600">{selectedProduct.name}</p>
-                          <div className="flex items-center gap-8 font-heading-2 text-black">
-                            <p>₩</p>
-                            <p>{selectedProduct.price.toLocaleString()}</p>
+                  <div className="flex flex-col gap-20">
+                    {/* Row 1: Name & Price */}
+                    <div className="w-400">
+                      {/* Name & Price */}
+                      <div className="flex flex-col gap-12">
+                        <p className="font-heading-1 text-blue-600">{selectedProduct.name}</p>
+                        <div className="flex items-center gap-8 font-heading-2 text-black">
+                          <p>₩</p>
+                          <p>{selectedProduct.price.toLocaleString()}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Row 2: Image + Specs */}
+                    <div className="flex items-start gap-56">
+                      {/* Image */}
+                      <div className="w-400 h-400 bg-gray-200 relative">
+                      </div>
+
+                      {/* Right Section - Specs */}
+                      <div className="w-303 flex flex-col gap-40">
+                        {/* Product Info Table */}
+                        <div className="flex flex-col gap-20 pl-16">
+                          <div className="flex items-center gap-24">
+                            <p className="font-body-2-r text-gray-400 w-80 flex-shrink-0">모델명</p>
+                            <p className="font-body-2-r text-black line-clamp-1">{selectedProduct.name}</p>
+                          </div>
+                          <div className="flex items-center gap-24">
+                            <p className="font-body-2-r text-gray-400 w-80">카테고리</p>
+                            <p className="font-body-2-r text-black">{selectedProduct.category}</p>
+                          </div>
+                          <div className="flex items-center gap-24">
+                            <p className="font-body-2-r text-gray-400 w-80">브랜드</p>
+                            <p className="font-body-2-r text-black">Apple</p>
+                          </div>
+                          <div className="flex items-center gap-24">
+                            <p className="font-body-2-r text-gray-400 w-80">색상</p>
+                            <p className="font-body-2-r text-black">내추럴 티타늄</p>
+                          </div>
+                          <div className="flex items-center gap-24">
+                            <p className="font-body-2-r text-gray-400 w-80">가격</p>
+                            <div className="flex items-center gap-4 font-body-2-r text-black">
+                              <p>{selectedProduct.price.toLocaleString()}</p>
+                              <p>원</p>
+                            </div>
+                          </div>
+                          <div className="flex items-center gap-24">
+                            <p className="font-body-2-r text-gray-400 w-80">충전방식</p>
+                            <p className="font-body-2-r text-black">USB-C</p>
+                          </div>
+                          <div className="flex items-center gap-24">
+                            <p className="font-body-2-r text-gray-400 w-80">출시일</p>
+                            <p className="font-body-2-r text-black">2023년 9월</p>
                           </div>
                         </div>
 
-                        {/* Image */}
-                        <div className="w-400 h-400 bg-gray-200 relative">
+                        {/* Hashtags */}
+                        <div className="flex items-center gap-16">
+                          <ProductLife label="office" />
+                          <ProductLife label="portability" />
                         </div>
                       </div>
+                    </div>
 
-                      {/* Button */}
+                    {/* Row 3: Button */}
+                    <div className="w-400">
                       <PrimaryButton
                         text={addToCombinationConfig.text}
                         onClick={addToCombinationConfig.handler}
                         disabled={isProfileLoading}
                         className="w-full bg-blue-500 hover:bg-blue-400 transition-colors"
                       />
-                    </div>
-
-                    {/* Right Section */}
-                    <div className="w-303 flex flex-col gap-40 pt-118">
-                      {/* Product Info Table */}
-                      <div className="flex flex-col gap-20 pl-16">
-                        <div className="flex items-center gap-24">
-                          <p className="font-body-2-r text-gray-400 w-80">모델명</p>
-                          <p className="font-body-2-r text-black">{selectedProduct.name}</p>
-                        </div>
-                        <div className="flex items-center gap-24">
-                          <p className="font-body-2-r text-gray-400 w-80">카테고리</p>
-                          <p className="font-body-2-r text-black">{selectedProduct.category}</p>
-                        </div>
-                        <div className="flex items-center gap-24">
-                          <p className="font-body-2-r text-gray-400 w-80">브랜드</p>
-                          <p className="font-body-2-r text-black">Apple</p>
-                        </div>
-                        <div className="flex items-center gap-24">
-                          <p className="font-body-2-r text-gray-400 w-80">색상</p>
-                          <p className="font-body-2-r text-black">내추럴 티타늄</p>
-                        </div>
-                        <div className="flex items-center gap-24">
-                          <p className="font-body-2-r text-gray-400 w-80">가격</p>
-                          <div className="flex items-center gap-4 font-body-2-r text-black">
-                            <p>{selectedProduct.price.toLocaleString()}</p>
-                            <p>원</p>
-                          </div>
-                        </div>
-                        <div className="flex items-center gap-24">
-                          <p className="font-body-2-r text-gray-400 w-80">충전방식</p>
-                          <p className="font-body-2-r text-black">USB-C</p>
-                        </div>
-                        <div className="flex items-center gap-24">
-                          <p className="font-body-2-r text-gray-400 w-80">출시일</p>
-                          <p className="font-body-2-r text-black">2023년 9월</p>
-                        </div>
-                      </div>
-
-                      {/* Hashtags */}
-                      <div className="flex items-center gap-16">
-                        <ProductLife label="office" />
-                        <ProductLife label="portability" />
-                      </div>
                     </div>
                   </div>
                 </div>
