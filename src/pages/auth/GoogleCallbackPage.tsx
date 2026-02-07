cb7c5bdc25bc597ea7900d2c545b759e768375fe
Seony777 <addiolsw1@gmail.com>
2026-02-07 22:56:17 +0900

    feat: 구글 콜백 페이지 추가 및 라우트 연결

---
+++ src/pages/auth/GoogleCallbackPage.tsx
@@ -0,0 +1,9 @@
+const GoogleCallbackPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[calc(100vh-80px)]">
+      <p className="font-body-2-r text-gray-600">로그인 처리 중...</p>
+    </div>
+  );
+};
+
+export default GoogleCallbackPage;
